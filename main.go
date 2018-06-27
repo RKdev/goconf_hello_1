d180051583765f586f01bccde76a1733a49cd943
keys <rkdevmail@gmail.com>
2018-06-27 07:50:55 -0400

    added main

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import stringtutils "goconf/goconf_mystringutils"
+import "fmt"
+
+func main() {
+	fmt.Println(stringtutils.Upper("Hello, World!"))
+	fmt.Println(stringtutils.Lower("Hello, World!"))
+}
